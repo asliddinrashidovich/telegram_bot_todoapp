6190a3ef8b635e1268300493e8058dbd85c573b1
Asliddin <asliddinrashidovich7@gmail.com>
2026-06-27 00:12:07 +0500

    some changes

--- src/migration-config.js
+++ src/migration-config.js
@@ -1,14 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  databaseUrl: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  },
-
+  databaseUrl: process.env.DATABASE_URL,
   migrationsTable: "pgmigrations",
   dir: "migrations",
   direction: "  up",
