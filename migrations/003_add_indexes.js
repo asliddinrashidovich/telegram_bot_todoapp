a00f6b5c7101d0f699f90baa06f70e21f23817c5
Asliddin <asliddinrashidovich7@gmail.com>
2026-06-27 00:07:54 +0500

    some changes

--- migrations/003_add_indexes.js
+++ migrations/003_add_indexes.js
@@ -1,9 +1,13 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  pgm.createIndex("tasks", ["user_id", "status"]);
+  pgm.createIndex("tasks", ["user_id", "status"], {
+    name: "tasks_user_status_idx",
+  });
 };
 
 exports.down = (pgm) => {
-  pgm.dropIndex("tasks", ["user_id", "status"]);
+  pgm.dropIndex("tasks", ["user_id", "status"], {
+    name: "tasks_user_status_idx",
+  });
 };
